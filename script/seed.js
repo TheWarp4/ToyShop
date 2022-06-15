586207ab120ed959ec86cf2f1454cd27ab9a1ae0
navis6927 <100895918+navis6927@users.noreply.github.com>
2022-06-15 16:35:50 -0400

    Admin Rights Added

--- script/seed.js
+++ script/seed.js
@@ -1,35 +1,81 @@
-'use strict'
+"use strict";
 
-const {db, models: {User, Product}} = require('../server/db')
+const {
+  db,
+  models: { User, Product },
+} = require("../server/db");
 
 /**
  * seed - this function clears the database, updates tables to
  *      match the models, and populates the database.
  */
 async function seed() {
-  await db.sync({ force: true }) // clears db and matches models to tables
-  console.log('db synced!')
+  await db.sync({ force: true }); // clears db and matches models to tables
+  console.log("db synced!");
 
   // Creating Users
   const users = await Promise.all([
-    User.create({ username: 'cody', password: '123', email:'cody123@gmail.com'}),
-    User.create({ username: 'murphy', password: '123', email:'murphy123@gmail.com'}),
-    Product.create({price: 49.99, description: 'Set Number 10717', category: 'LEGOS', image: 'https://images-na.ssl-images-amazon.com/images/I/71C-nuAyCPL.__AC_SY300_SX300_QL70_FMwebp_.jpg', productName: 'LEGO Classics Building Blocks'}),
-    Product.create({price: 19.99, description: 'Barbie Dreamtopia', category: 'BARBIE', image: 'https://albaniandubs.weebly.com/uploads/5/7/8/2/57825701/barbi-e-liqenit-te-mjellmave-2003-albanian-dubs_1.jpg', productName: 'Barbie - Liqenit te Mjellmave'}),
-    Product.create({price: 74.99, description: 'Jurassic TREX Colossal Figure for Ages 12+', category: 'JURASSIC', image: 'https://i5.walmartimages.com/asr/b9ee23c9-d147-4853-98c7-81d539f4eb28.3aabe75528efac54c112ee83edfd1be2.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF', productName: 'Jurassic World - TREX FIGURE'}),
-    Product.create({price: 22.99, description: 'Optimus Prime Figure for Ages 8+', category: 'TRANSFORMERS', image: 'https://m.media-amazon.com/images/I/81NZSeXQHDL._AC_SX522_.jpg', productName: 'Transformers - Optimus Prime Figure'}),
-    Product.create({price: 14.99, description: 'Soft Stuffed Animal', category: 'STUFFED ANIMALS', image:'https://images-na.ssl-images-amazon.com/images/I/51Tahu98IiL.__AC_SY300_SX300_QL70_FMwebp_.jpg', productName: 'Squishmallow'}),
+    User.create({
+      username: "cody",
+      password: "123",
+      email: "cody123@gmail.com",
+      type: 'admin'
+    }),
+    User.create({
+      username: "murphy",
+      password: "123",
+      email: "murphy123@gmail.com",
+      type: 'customer'
+    }),
+    Product.create({
+      price: 49.99,
+      description: "Set Number 10717",
+      category: "LEGOS",
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/71C-nuAyCPL.__AC_SY300_SX300_QL70_FMwebp_.jpg",
+      productName: "LEGO Classics Building Blocks",
+    }),
+    Product.create({
+      price: 19.99,
+      description: "Barbie Dreamtopia",
+      category: "BARBIE",
+      image:
+        "https://albaniandubs.weebly.com/uploads/5/7/8/2/57825701/barbi-e-liqenit-te-mjellmave-2003-albanian-dubs_1.jpg",
+      productName: "Barbie - Liqenit te Mjellmave",
+    }),
+    Product.create({
+      price: 74.99,
+      description: "Jurassic TREX Colossal Figure for Ages 12+",
+      category: "JURASSIC",
+      image:
+        "https://i5.walmartimages.com/asr/b9ee23c9-d147-4853-98c7-81d539f4eb28.3aabe75528efac54c112ee83edfd1be2.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
+      productName: "Jurassic World - TREX FIGURE",
+    }),
+    Product.create({
+      price: 22.99,
+      description: "Optimus Prime Figure for Ages 8+",
+      category: "TRANSFORMERS",
+      image: "https://m.media-amazon.com/images/I/81NZSeXQHDL._AC_SX522_.jpg",
+      productName: "Transformers - Optimus Prime Figure",
+    }),
+    Product.create({
+      price: 14.99,
+      description: "Soft Stuffed Animal",
+      category: "STUFFED ANIMALS",
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/51Tahu98IiL.__AC_SY300_SX300_QL70_FMwebp_.jpg",
+      productName: "Squishmallow",
+    }),
+  ]);
 
-  ])
-
-  console.log(`seeded ${users.length} users`)
-  console.log(`seeded successfully`)
+  console.log(`seeded ${users.length} users`);
+  console.log(`seeded successfully`);
   return {
     users: {
       cody: users[0],
-      murphy: users[1]
-    }
-  }
+      murphy: users[1],
+    },
+  };
 }
 
 /*
@@ -38,16 +84,16 @@ async function seed() {
  The `seed` function is concerned only with modifying the database.
 */
 async function runSeed() {
-  console.log('seeding...')
+  console.log("seeding...");
   try {
-    await seed()
+    await seed();
   } catch (err) {
-    console.error(err)
-    process.exitCode = 1
+    console.error(err);
+    process.exitCode = 1;
   } finally {
-    console.log('closing db connection')
-    await db.close()
-    console.log('db connection closed')
+    console.log("closing db connection");
+    await db.close();
+    console.log("db connection closed");
   }
 }
 
@@ -57,8 +103,8 @@ async function runSeed() {
   any errors that might occur inside of `seed`.
 */
 if (module === require.main) {
-  runSeed()
+  runSeed();
 }
 
 // we export the seed function for testing purposes (see `./seed.spec.js`)
-module.exports = seed
+module.exports = seed;
