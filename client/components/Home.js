db9a13fee81ed8ee9196e170a865f765269b9768
Angel J. Gomez <angelgomez144@icloud.com>
2022-06-17 17:08:57 -0400

    AddProduct component

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,29 +1,31 @@
-import React from 'react'
-import {connect} from 'react-redux'
+import React from "react";
+import { connect } from "react-redux";
+import AddProduct from "./AddProduct";
 
 /**
  * COMPONENT
  */
-export const Home = props => {
-  const {username} = props
-  const {id} = props
-  console.log(props.id)
+export const Home = (props) => {
+  const { username } = props;
+  const { id } = props;
+  console.log(props.id);
 
   return (
     <div>
       <h3>Welcome, {username}</h3>
+      <AddProduct />
     </div>
-  )
-}
+  );
+};
 
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapState = (state) => {
   return {
     username: state.auth.username,
-    id: state.auth.id
-  }
-}
+    id: state.auth.id,
+  };
+};
 
-export default connect(mapState)(Home)
+export default connect(mapState)(Home);
