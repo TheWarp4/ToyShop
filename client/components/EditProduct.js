a70248073c5a49febc6f704ee410141680672edd
Angel J. Gomez <angelgomez144@icloud.com>
2022-06-18 17:08:28 -0400

    AdminComponent

---
+++ client/components/EditProduct.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const EditProduct = (props) => {
+  const [product, setProduct] = useState({
+    productName: "",
+    price: 0,
+    category: "",
+    image: "",
+    description: "",
+  });
+
+  useEffect(() => {
+
+  })
+
+  const handleSubmit = async function (event) {
+    try {
+      event.preventDefault();
+      const { data } = await axios.put("/api/products", product);
+      setProduct({
+        productName: "",
+        price: 0,
+        category: "",
+        image: "",
+        description: "",
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleChange = (event) => {
+    setProduct({ ...product, [event.target.name]: event.target.value });
+  };
+
+  return (
+    <form id="product-form" onSubmit={handleSubmit}>
+      <label htmlFor="productName">Product Name:</label>
+      <input
+        name="productName"
+        onChange={handleChange}
+        value={product.productName}
+      />
+
+      <label htmlFor="price">Price:</label>
+      <input name="price" onChange={handleChange} value={product.price} />
+
+      <label htmlFor="category">Category:</label>
+      <select name="category" onChange={handleChange} value={product.category}>
+        <option value="LEGOS">LEGOS</option>
+        <option value="TRANSFORMERS">TRANSFORMERS</option>
+        <option value="JURASSIC">JURASSIC</option>
+      </select>
+
+      <label htmlFor="image"></label>
+      <input name="image" onChange={handleChange} value={product.image} />
+
+      <label htmlFor="description">Description:</label>
+      <input
+        name="description"
+        onChange={handleChange}
+        value={product.description}
+      />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default EditProduct;
--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,37 +1,36 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import EditProduct from "./EditProduct";
 
 function SingleProduct(props) {
   // LOCAL STATE
-  const [product, setProduct] = useState({})
+  const [product, setProduct] = useState({});
 
   // FETCH SINGLE PRODUCT AXIOS REQ
   const fetchProduct = async (id) => {
     try {
-      const {data} = await axios.get(`/api/products/${id}`)
-      setProduct(data.singleProduct)
+      const { data } = await axios.get(`/api/products/${id}`);
+      setProduct(data.singleProduct);
+    } catch (error) {
+      console.log(error);
     }
-    catch (error) {
-      console.log(error)
-    }
-  }
-
+  };
 
   // USE EFFECT (OR COMPDIDMOUNT)
   useEffect(() => {
-    fetchProduct(props.match.params.id)
-  }, [])
+    fetchProduct(props.match.params.id);
+  }, []);
 
+  console.log("THIS IS PRODUCT", product);
   return (
-    <div className = 'single-product'>
-      <img src = {product.image} />
+    <div className="single-product">
+      <img src={product.image} />
       <div>Name: {product.productName}</div>
       <div>Description: {product.description}</div>
       <div>Category: {product.category}</div>
       <div>Price: {product.price}</div>
     </div>
-  )
+  );
 }
 
-export default SingleProduct
+export default SingleProduct;
