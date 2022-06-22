f0f050bd96286cb2051d2259e03006900f4a6152
Kevin Yu <Kevin.Yu.2468@gmail.com>
2022-06-22 09:50:17 -0700

    Fix rerendering in cart

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -4,93 +4,65 @@ import { connect } from "react-redux";
 
 const Cart = (props) => {
   const [shoppingCart, setShoppingCart] = useState([]);
-  const [total, setTotal] = useState(0.00);
-  const fetchCartFromLocalStorage = JSON.parse(window.localStorage.getItem('cart') || '[]')
+  const [total, setTotal] = useState(0.0);
+  const fetchCartFromLocalStorage = JSON.parse(
+    window.localStorage.getItem("cart") || "[]"
+  );
 
   const [cart, setCart] = useState(fetchCartFromLocalStorage);
 
   const mergeLocalCart = async (userId) => {
     try {
-      const getOrderSessionId = await axios.get(`/api/ordersessions/${userId}`)
-      const {data} = await axios.get(`/api/shoppingcarts/${getOrderSessionId.data.id}`)
+      const getOrderSessionId = await axios.get(`/api/ordersessions/${userId}`);
+      const { data } = await axios.get(
+        `/api/shoppingcarts/${getOrderSessionId.data.id}`
+      );
       cart.map(async (prodData) => {
         await axios.post(`/api/shoppingcarts`, {
           orderSessionId: getOrderSessionId.data.id,
           productId: prodData.id,
           itemQuantity: prodData.itemQuantity,
-        })
-      })
-      localStorage.setItem('cart', JSON.stringify([]))
-
+        });
+      });
+      localStorage.setItem("cart", JSON.stringify([]));
+    } catch (err) {
+      console.log(err);
     }
-      catch (err){
-        console.log(err)
-      }
-    }
+  };
 
   const fetchShoppingCart = async (userId) => {
     try {
-      const getOrderSessionId = await axios.get(`/api/ordersessions/${userId}`)
-      const {data} = await axios.get(`/api/shoppingcarts/${getOrderSessionId.data.id}`)
-      data.map( async (prodData) => {
-        const productInfo = await axios.get(`/api/products/${prodData.productId}`)
-        productInfo.data.singleProduct.itemQuantity = prodData.itemQuantity
-        setShoppingCart(prevCart => [...prevCart, productInfo.data.singleProduct])
-        setTotal(prevTotal => prevTotal + parseFloat(productInfo.data.singleProduct.price)*prodData.itemQuantity)
-      })
+      const getOrderSessionId = await axios.get(`/api/ordersessions/${userId}`);
+      const { data } = await axios.get(
+        `/api/shoppingcarts/${getOrderSessionId.data.id}`
+      );
+      data.map(async (prodData) => {
+        const productInfo = await axios.get(
+          `/api/products/${prodData.productId}`
+        );
+        productInfo.data.singleProduct.itemQuantity = prodData.itemQuantity;
+        setShoppingCart((prevCart) => [
+          ...prevCart,
+          productInfo.data.singleProduct,
+        ]);
+        setTotal(
+          (prevTotal) =>
+            prevTotal +
+            parseFloat(productInfo.data.singleProduct.price) *
+              prodData.itemQuantity
+        );
+      });
     } catch (error) {
       console.log(error);
     }
   };
 
-  const deleteFromShoppingCart = async (userId, productId) => {
-    const getOrderSessionId = await axios.get(`/api/ordersessions/${userId}`);
-    await axios.delete(
-      `/api/shoppingcarts/${getOrderSessionId.data.id}/${productId}`
-    );
-    setShoppingCart([]);
-    setTotal(0.0);
-    fetchShoppingCart(props.userId);
-  };
-
-  const handleDecrement = async (userId, productId) => {
-    const getOrderSessionId = await axios.get(`/api/ordersessions/${userId}`);
-    const {data} = await axios.get(`/api/shoppingcarts/${getOrderSessionId.data.id}`)
-    const [foundProduct] = data.filter((product)=> product.productId == productId)
-    if (foundProduct.itemQuantity == 1) {
-      await axios.delete(
-        `/api/shoppingcarts/${getOrderSessionId.data.id}/${productId}`
-      );
-      setShoppingCart([]);
-      setTotal(0.0);
+  useEffect(() => {
+    if (props.userId) {
+      mergeLocalCart(props.userId);
       fetchShoppingCart(props.userId);
     }
-    else {
-          await axios.put(
-      `/api/shoppingcarts/${getOrderSessionId.data.id}/${productId}/decrement`
-    );
-    setShoppingCart([]);
-    setTotal(0.0);
-    fetchShoppingCart(props.userId);
-    }
-  };
-
-  const handleIncrement = async (userId, productId) => {
-    const getOrderSessionId = await axios.get(`/api/ordersessions/${userId}`);
-    await axios.put(
-      `/api/shoppingcarts/${getOrderSessionId.data.id}/${productId}/increment`
-    );
-    setShoppingCart([]);
-    setTotal(0.0);
-    fetchShoppingCart(props.userId);
-  };
-
-  useEffect(() => {
-    if(props.userId) {
-      mergeLocalCart(props.userId);
-      fetchShoppingCart(props.userId)
-    }
-  }, [props.userId, cart])
+  }, [props.userId, cart]);
 
   return (
     <div id="cart-container">
@@ -98,56 +70,24 @@ const Cart = (props) => {
       <div className="shopping-cart">
         {shoppingCart.map((product) => (
           <div key={product.id}>
-            <div className="product-info-container">
-              <img
-                className="sc-photo"
-                src={`${product.image}`}
-                onClick={() => {
-                  location.href = `/products/${product.id}`;
-                }}
-              />
-              <div className="product-details">
-                <div>
-                  <div className="sc-product-name">{product.productName}</div>
-                  <div className="sc-quantity">
-                    <button
-                      className="sc-incrementer"
-                      onClick={() => {
-                        handleDecrement(props.userId, product.id);
-                      }}
-                    >
-                      -
-                    </button>
-                    {product.itemQuantity}
-                    <button
-                      className="sc-incrementer"
-                      onClick={() => {
-                        handleIncrement(props.userId, product.id);
-                      }}
-                    >
-                      +
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <div>${product.price}</div>
-              <button
-                className="delete-sc-item"
-                onClick={() => deleteFromShoppingCart(props.userId, product.id)}
-              >
-                X
-              </button>
-            </div>
+            <SingleProductInCart
+              product={product}
+              userId={props.userId}
+              setTotal={setTotal}
+              total={total}
+            />
             <hr className="sc-horizontal-line" />
           </div>
         ))}
         <div className="sc-subtotal-checkout">
-          <div className="sc-subtotal">SubTotal: ${total.toFixed(2)}</div>
+          <div className="sc-subtotal single-product-info">
+            SubTotal: ${total.toFixed(2)}
+          </div>
           <button
             className="sc-checkout-btn"
             onClick={() => {
               if (shoppingCart.length >= 1) {
-              location.href = `/checkout`;
+                location.href = `/checkout`;
               }
             }}
           >
@@ -156,6 +96,98 @@ const Cart = (props) => {
         </div>
       </div>
     </div>
+  );
+};
+
+const SingleProductInCart = ({ product, userId, setTotal, total }) => {
+  const [productItemQuantity, setProductItemQuantity] = useState(
+    product.itemQuantity
+  );
+  const [isProductExist, setIsProductExist] = useState(true);
+  const deleteFromShoppingCart = async (userId, productId) => {
+    const getOrderSessionId = await axios.get(`/api/ordersessions/${userId}`);
+    await axios.delete(
+      `/api/shoppingcarts/${getOrderSessionId.data.id}/${productId}`
+    );
+    setTotal(total - product.price * productItemQuantity);
+    setIsProductExist(false);
+  };
+
+  const handleDecrement = async (userId, productId) => {
+    setProductItemQuantity(productItemQuantity - 1);
+
+    const getOrderSessionId = await axios.get(`/api/ordersessions/${userId}`);
+    const { data } = await axios.get(
+      `/api/shoppingcarts/${getOrderSessionId.data.id}`
+    );
+    const [foundProduct] = data.filter(
+      (product) => product.productId == productId
+    );
+    if (productItemQuantity == 1) {
+      await axios.delete(
+        `/api/shoppingcarts/${getOrderSessionId.data.id}/${productId}`
+      );
+      setIsProductExist(false);
+    } else {
+      await axios.put(
+        `/api/shoppingcarts/${getOrderSessionId.data.id}/${productId}/decrement`
+      );
+    }
+    setTotal(total - product.price);
+  };
+
+  const handleIncrement = async (userId, productId) => {
+    setProductItemQuantity(productItemQuantity + 1);
+    const getOrderSessionId = await axios.get(`/api/ordersessions/${userId}`);
+    await axios.put(
+      `/api/shoppingcarts/${getOrderSessionId.data.id}/${productId}/increment`
+    );
+    setTotal(total + product.price);
+  };
+
+  return (
+    !isProductExist || (
+      <div className="product-info-container">
+        <img
+          className="sc-photo"
+          src={`${product.image}`}
+          onClick={() => {
+            location.href = `/products/${product.id}`;
+          }}
+        />
+        <div className="product-details">
+          <div>
+            <div className="sc-product-name">{product.productName}</div>
+            <div className="sc-quantity">
+              <button
+                className="sc-incrementer"
+                onClick={() => {
+                  handleDecrement(userId, product.id);
+                }}
+              >
+                -
+              </button>
+              {productItemQuantity}
+              <button
+                className="sc-incrementer"
+                onClick={() => {
+                  handleIncrement(userId, product.id);
+                }}
+              >
+                +
+              </button>
+            </div>
+          </div>
+        </div>
+        <div>${product.price}</div>
+        <button
+          className="delete-sc-item"
+          onClick={() => deleteFromShoppingCart(userId, product.id)}
+        >
+          X
+        </button>
+      </div>
+    )
   );
 };
 
--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -5,47 +5,58 @@ import axios from "axios";
 import { connect } from "react-redux";
 
 function Checkout(props) {
-
-  const fetchCartFromLocalStorage = JSON.parse(window.localStorage.getItem('cart') || '[]')
+  const fetchCartFromLocalStorage = JSON.parse(
+    window.localStorage.getItem("cart") || "[]"
+  );
   const [cart, setCart] = useState(fetchCartFromLocalStorage);
-  const [total, setTotal] = useState(0.00);
+  const [total, setTotal] = useState(0.0);
   const [shoppingCart, setShoppingCart] = useState([]);
-  const [emailValue, setEmailValue] = useState('')
+  const [emailValue, setEmailValue] = useState("");
 
+  const fetchLocalCart = () => {
+    cart.map((localProduct) => {
+      setShoppingCart((prevCart) => [...prevCart, localProduct]);
+      setTotal(
+        (prevTotal) =>
+          prevTotal + parseFloat(localProduct.price) * localProduct.itemQuantity
+      );
+    });
+  };
 
-const fetchLocalCart = () => {
-  cart.map((localProduct) => {
-    setShoppingCart(prevCart => [...prevCart, localProduct])
-    setTotal(prevTotal => prevTotal + parseFloat(localProduct.price)*localProduct.itemQuantity)
-  })
-}
-
-const handleSavingInfo = () => {
-  localStorage.setItem("email", emailValue);
-
-}
-
+  const handleSavingInfo = () => {
+    localStorage.setItem("email", emailValue);
+  };
 
   const fetchShoppingCart = async (userId) => {
     try {
-      const getOrderSessionId = await axios.get(`/api/ordersessions/${userId}`)
-      const {data} = await axios.get(`/api/shoppingcarts/${getOrderSessionId.data.id}`)
-      data.map( async (prodData) => {
-        const productInfo = await axios.get(`/api/products/${prodData.productId}`)
+      const getOrderSessionId = await axios.get(`/api/ordersessions/${userId}`);
+      const { data } = await axios.get(
+        `/api/shoppingcarts/${getOrderSessionId.data.id}`
+      );
+      data.map(async (prodData) => {
+        const productInfo = await axios.get(
+          `/api/products/${prodData.productId}`
+        );
         productInfo.data.singleProduct.itemQuantity = prodData.itemQuantity;
-        setShoppingCart(prevCart => [...prevCart, productInfo.data.singleProduct])
-        setTotal(prevTotal => prevTotal + parseFloat(productInfo.data.singleProduct.price)*prodData.itemQuantity)
-      })
-
+        setShoppingCart((prevCart) => [
+          ...prevCart,
+          productInfo.data.singleProduct,
+        ]);
+        setTotal(
+          (prevTotal) =>
+            prevTotal +
+            parseFloat(productInfo.data.singleProduct.price) *
+              prodData.itemQuantity
+        );
+      });
     } catch (error) {
       console.log(error);
     }
   };
   useEffect(() => {
-    fetchShoppingCart(props.userId)
-    fetchLocalCart()
-
-  }, [])
+    fetchShoppingCart(props.userId);
+    fetchLocalCart();
+  }, []);
 
   return (
     <div id="gc-container">
@@ -54,112 +65,115 @@ const handleSavingInfo = () => {
           <div className="c-contact-info-title">Contact Information</div>
         </div>
         <form
-        method="POST"
-        action="/checkout/payment"
-        onSubmit={(e) => {handleSavingInfo(e)}}
-        >
-
-        <input
-          className="gc-email-input"
-          placeholder="Email"
-          type="email"
-          required
-          value={emailValue}
-          onChange = {(e) => {
-            setEmailValue(e.target.value)
+          method="POST"
+          action="/checkout/payment"
+          onSubmit={(e) => {
+            handleSavingInfo(e);
           }}
-        />
-
-        <div className="gc-shipping-address-title">Shipping Address</div>
-        <div className="gc-name-input">
+        >
           <input
-            className="gc-first-name"
-            placeholder="First name"
+            className="gc-email-input"
+            placeholder="Email"
+            type="email"
+            required
+            value={emailValue}
+            onChange={(e) => {
+              setEmailValue(e.target.value);
+            }}
+          />
+
+          <div className="gc-shipping-address-title">Shipping Address</div>
+          <div className="gc-name-input">
+            <input
+              className="gc-first-name"
+              placeholder="First name"
+              type="text"
+              required
+            ></input>
+            <input
+              className="gc-last-name"
+              placeholder="Last name"
+              type="text"
+              required
+            ></input>
+          </div>
+          <input
+            className="gc-address-input"
+            placeholder="Address"
             type="text"
             required
           ></input>
           <input
-            className="gc-last-name"
-            placeholder="Last name"
+            className="gc-address-line2-input"
+            placeholder="Apartment, suite, etc. (optional)"
             type="text"
-            required
           ></input>
-        </div>
-        <input
-          className="gc-address-input"
-          placeholder="Address"
-          type="text"
-          required
-
-        ></input>
-        <input
-          className="gc-address-line2-input"
-          placeholder="Apartment, suite, etc. (optional)"
-          type="text"
-        ></input>
-        <input className="gc-city-input" placeholder="City" type="text" required></input>
-        <div className="gc-country-state-zip-input">
-          <select id="gc-country-input" name="country" type="text">
-            <option>United States</option>
-          </select>
-          <select id="gc-state-input" type="text" required>
-            <option>State</option>
-            {state.map((state, i) => (
-              <option key={`gc-${i}`}>{state}</option>
-            ))}
-          </select>
           <input
-            id="gc-zipcode-input"
-            placeholder="ZIP Code"
+            className="gc-city-input"
+            placeholder="City"
             type="text"
-            minLength='5'
-            maxLength='5'
-
             required
           ></input>
-        </div>
-        <input
-          className="gc-phone-input"
-          placeholder="Phone"
-          type="tel"
-          minLength='10'
-            maxLength='11'
-          required
-        ></input>
-        <a href="/checkout/payment">
-          <input type='submit' value='Continue to Payment' className="gc-payment-btn" ></input>
-        </a>
+          <div className="gc-country-state-zip-input">
+            <select id="gc-country-input" name="country" type="text">
+              <option>United States</option>
+            </select>
+            <select id="gc-state-input" type="text" required>
+              <option>State</option>
+              {state.map((state, i) => (
+                <option key={`gc-${i}`}>{state}</option>
+              ))}
+            </select>
+            <input
+              id="gc-zipcode-input"
+              placeholder="ZIP Code"
+              type="text"
+              minLength="5"
+              maxLength="5"
+              required
+            ></input>
+          </div>
+          <input
+            className="gc-phone-input"
+            placeholder="Phone"
+            type="tel"
+            minLength="10"
+            maxLength="11"
+            required
+          ></input>
+          <a href="/checkout/payment">
+            <input
+              type="submit"
+              value="Continue to Payment"
+              className="gc-payment-btn"
+            ></input>
+          </a>
         </form>
       </div>
 
-      <hr className="gc-container-divider"/>
+      <hr className="gc-container-divider" />
 
       <div className="container-right">
         <div className="gc-shopping-cart">Shopping Cart</div>
-            {shoppingCart.map((product, i) => (
-              <div key={i}>
-              <div className="gc-product-container">
-                <img className= 'gc-product-photo' src = {product.image}/>
-                <div>
-                {product.productName}
-                </div>
-                <div>
-                  ${product.price*product.itemQuantity}
-                </div>
-              </div>
-              <hr/>
-              </div>
-            ))}
-            <div id="gc-subtotal-total-price">
-              <div>Subtotal</div>
-            <div>${total.toFixed(2)}</div>
+        {shoppingCart.map((product, i) => (
+          <div key={i}>
+            <div className="gc-product-container">
+              <img className="gc-product-photo" src={product.image} />
+              <div>{product.productName}</div>
+              <div>${(product.price * product.itemQuantity).toFixed(2)}</div>
             </div>
-            <hr/>
-            <div id="gc-subtotal-total-price">
-              <div>Total</div>
-            <div>${total.toFixed(2)}USD</div>
-            </div>
-
+            <hr />
+          </div>
+        ))}
+        <div id="gc-subtotal-total-price">
+          <div>Subtotal</div>
+          <div>${total.toFixed(2)}</div>
+        </div>
+        <hr />
+        <div id="gc-subtotal-total-price">
+          <div>Total</div>
+          <div>${total.toFixed(2)}USD</div>
+        </div>
       </div>
     </div>
   );
