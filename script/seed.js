58e616b1ecd47d987fafee901afedbc0ad194599
Joey D <rjdykes1010805@outlook.com>
2022-06-22 07:26:14 -0700

    seeded product data

--- script/seed.js
+++ script/seed.js
@@ -4,15 +4,6 @@ faker.commerce.productDescription();
 faker.commerce.price(100, 200);
 faker.commerce.productName();
 
-// "LEGOS" https://www.pennlive.com/resizer/W-M-ACGvuLR-NwYnQmIBGLhivBk=/800x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/XE6N3UD2RJEU5LS6Q6C7K2PUTA.jpg
-
-// BARBIE https://www.rd.com/wp-content/uploads/2015/09/GettyImages-459000267-2-scaled.jpg?resize=768,512
-
-// JURASSIC https://m.media-amazon.com/images/I/71uD-zJwM3S._AC_SX425_.jpg
-
-// TRANSFORMERS https://m.media-amazon.com/images/I/61-Vf1z+fSL._AC_SX425_.jpg
-
-// STUFFED ANIMALS https://m.media-amazon.com/images/I/71oNgt0-dYL._AC_SX425_.jpg
 
 ("use strict");
 
@@ -85,7 +76,7 @@ const populateProductData = async () => {
     }),
     Product.create({
       price: 74.99,
-      description: "Jurassic TREX Colossal Figure for Ages 12+",
+      description: "Jurassic TREX Colossal",
       category: "JURASSIC",
       image:
         "https://i5.walmartimages.com/asr/b9ee23c9-d147-4853-98c7-81d539f4eb28.3aabe75528efac54c112ee83edfd1be2.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
@@ -93,7 +84,7 @@ const populateProductData = async () => {
     }),
     Product.create({
       price: 22.99,
-      description: "Optimus Prime Figure for Ages 8+",
+      description: "Optimus Prime",
       category: "TRANSFORMERS",
       image: "https://m.media-amazon.com/images/I/81NZSeXQHDL._AC_SX522_.jpg",
       productName: "Transformers - Optimus Prime Figure",
@@ -111,23 +102,79 @@ const populateProductData = async () => {
   const productDummyData = [
     [
       "LEGOS",
-      "https://www.pennlive.com/resizer/W-M-ACGvuLR-NwYnQmIBGLhivBk=/800x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/XE6N3UD2RJEU5LS6Q6C7K2PUTA.jpg",
+      "https://m.media-amazon.com/images/I/81odce4aScL._AC_UL320_.jpg",
+      "https://m.media-amazon.com/images/I/81vaB9d1pxL._AC_SX425_.jpg",
+      "https://m.media-amazon.com/images/I/81ZwS4MhuoL._AC_SX425_.jpg",
+      "https://m.media-amazon.com/images/I/81NCFZ3iJdL._AC_SX425_.jpg",
+      "https://m.media-amazon.com/images/I/91RaDysK8zL._AC_SX425_.jpg",
+      "https://m.media-amazon.com/images/I/81OqSTy+A1L._AC_SX425_.jpg",
+      "https://m.media-amazon.com/images/I/81LO+S3rrvL._AC_SX425_.jpg",
+      "https://m.media-amazon.com/images/I/71WmJVAhVIL._AC_SX425_.jpg",
+      "https://m.media-amazon.com/images/I/61aLF2MdqIL._AC_SX425_.jpg",
+      "https://m.media-amazon.com/images/I/816F+MPVeCL._AC_SX425_.jpg",
+      "https://m.media-amazon.com/images/I/81AhTmzbr2L._AC_SX425_.jpg",
+      "https://m.media-amazon.com/images/I/81bsR+YCBwL._AC_SX425_.jpg"
     ],
     [
       "BARBIE",
       "https://www.rd.com/wp-content/uploads/2015/09/GettyImages-459000267-2-scaled.jpg?resize=768,512",
+      "https://m.media-amazon.com/images/I/81qpWhtgu6L._AC_SX425_.jpg",
+      "https://m.media-amazon.com/images/I/81peZfLxCNL._AC_SX425_.jpg",
+      "https://m.media-amazon.com/images/I/61hWRyCZDQL._AC_SY741_.jpg",
+      "https://m.media-amazon.com/images/I/71cpnUKTepL._AC_SX569_.jpg",
+      "https://m.media-amazon.com/images/I/71QclTR9FQL._AC_SX425_.jpg",
+      "https://images-na.ssl-images-amazon.com/images/I/71cM3H3vfcL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+      "https://m.media-amazon.com/images/I/81owpGnSI-L._AC_SX425_.jpg",
+      "https://m.media-amazon.com/images/I/81PAxoSDVxL._AC_SX425_.jpg",
+      "https://m.media-amazon.com/images/I/61pEj-1uNOL._AC_SY550_.jpg",
+      "https://images-na.ssl-images-amazon.com/images/I/71nASv4rbsL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+      "https://m.media-amazon.com/images/I/91r0Smo920L._AC_SY450_.jpg"
+
     ],
     [
       "JURASSIC",
       "https://m.media-amazon.com/images/I/71uD-zJwM3S._AC_SX425_.jpg",
+"https://m.media-amazon.com/images/I/61OIdq73gNL._AC_SX425_.jpg",
+"https://m.media-amazon.com/images/I/81DuaOEhB-L._AC_SX425_.jpg",
+"https://m.media-amazon.com/images/I/71ckmeGmAhL._AC_SX425_.jpg",
+"https://media.kohlsimg.com/is/image/kohls/3788498?wid=1200&hei=1200&op_sharpen=1",
+"https://www.tradeinn.com/f/13866/138666426/jurassic-world-the-wild-chompin-carnotaurus-toro-dinosaur-toy-includes-various-actions-and-realistic-details-to-entertain.jpg",
+"https://images.immediate.co.uk/production/volatile/sites/4/2021/12/Best-dinosaur-toys-Robotime-Stegosaurus-b5a70cd.jpg?quality=90&webp=true&resize=975,730",
+"https://images.immediate.co.uk/production/volatile/sites/4/2021/12/Best-dinosaur-toys-Wild-Republic-Diplodocus-hand-puppet-8b753ab.jpg?quality=90&webp=true&resize=975,730",
+"https://images.immediate.co.uk/production/volatile/sites/4/2021/12/Best-dinosaur-toys-Super-Colossal-Indominus-Rex-86df8b6.jpg?quality=90&webp=true&resize=975,964",
+"https://images.immediate.co.uk/production/volatile/sites/4/2021/12/Best-dinosaur-toys-Stomp-%E2%80%98N-Escape-Tyrannosaurus-Rex-ba582e3.jpg?quality=90&webp=true&resize=975,450",
+"https://images.immediate.co.uk/production/volatile/sites/4/2021/12/Best-dinosaur-toys-Roar-Attack-Ankylosaurus--e216d8e.jpg?quality=90&webp=true&resize=975,473",
+"https://images.immediate.co.uk/production/volatile/sites/4/2021/12/Best-dinosaur-toys-Jurassic-World-Mosasaurus-7200522.jpg?quality=90&webp=true&resize=975,617",
     ],
     [
       "TRANSFORMERS",
       "https://m.media-amazon.com/images/I/61-Vf1z+fSL._AC_SX425_.jpg",
+"https://m.media-amazon.com/images/I/71H+nPUR9GL._AC_SX425_.jpg",
+"https://m.media-amazon.com/images/I/81umDrKV6CL._AC_SX425_.jpg",
+"https://m.media-amazon.com/images/I/71PiFCnubXL._AC_UL320_.jpg",
+"https://m.media-amazon.com/images/I/71NEKEXznAL._AC_UL320_.jpg",
+"https://m.media-amazon.com/images/I/71sQfw+UWJL._AC_UL320_.jpg",
+"https://m.media-amazon.com/images/I/719x3rDYwmL._AC_UL320_.jpg",
+"https://m.media-amazon.com/images/I/71VgVUCDtRL._AC_UL320_.jpg",
+"https://m.media-amazon.com/images/I/81GzHuzzg8L._AC_UL320_.jpg",
+"https://m.media-amazon.com/images/I/81FkPwfnghL._AC_UL320_.jpg",
+"https://m.media-amazon.com/images/I/812Bk3M9dwL._AC_UL320_.jpg",
+"https://m.media-amazon.com/images/I/71y0udORxrL._AC_UL320_.jpg",
     ],
     [
       "STUFFED ANIMALS",
       "https://m.media-amazon.com/images/I/71oNgt0-dYL._AC_SX425_.jpg",
+      "https://m.media-amazon.com/images/I/614GIhMwuOL._AC_UL320_.jpg",
+      "https://m.media-amazon.com/images/I/71Q5QrpltHL._AC_UL320_.jpg",
+      "https://m.media-amazon.com/images/I/51AUJAfsebL._AC_UL320_.jpg",
+      "https://m.media-amazon.com/images/I/61xTwDFMZRL._AC_UL320_.jpg",
+      "https://m.media-amazon.com/images/I/817YPdmbxJL._AC_UL320_.jpg",
+      "https://m.media-amazon.com/images/I/517v+-M4sNL._AC_UL320_.jpg",
+      "https://m.media-amazon.com/images/I/410+Lj2sMDS._AC_UL320_.jpg",
+      "https://m.media-amazon.com/images/I/810MV8hNM5L._AC_UL320_.jpg",
+      "https://m.media-amazon.com/images/I/71wYUOHA0AL._AC_UL320_.jpg",
+      "https://m.media-amazon.com/images/I/51Fwh33bUxS._AC_UL320_.jpg",
+      "https://m.media-amazon.com/images/I/71dgzrodFfS._AC_UL320_.jpg",
     ],
   ];
 
@@ -137,11 +184,12 @@ const populateProductData = async () => {
       const price = faker.commerce.price(5, 100);
       const productName = faker.commerce.productName();
       let randomCatagoryNumber = Math.floor(Math.random() * 5);
+      let randomUrlIndex = Math.floor((Math.random() * 12) + 1)
       Product.create({
         price: price,
         description: description,
         category: productDummyData[randomCatagoryNumber][0],
-        image: productDummyData[randomCatagoryNumber][1],
+        image: productDummyData[randomCatagoryNumber][randomUrlIndex],
         productName: productName,
       });
     }
@@ -155,11 +203,7 @@ const populateShoppingCartData = async () => {
   let threeUniqueProductIds = () => {
     let productNumbersArray = [];
     while (productNumbersArray.length < 3) {
-      // console.log(i)
-      // console.log(productNumbersArray)
       let randomProductIndex = Math.floor(Math.random() * 199) + 1;
-      // console.log(randomProductIndex)
-      // console.log((productNumbersArray.includes(randomProductIndex)))
       if (productNumbersArray.includes(randomProductIndex)) {
         continue;
       } else {
@@ -219,6 +263,9 @@ async function seed() {
   await user2.addOrderSession(orderSession2);
 
   console.log(`seeded lotsa users`);
+  console.log(`... and products`);
+  console.log(`... and carts`);
+  console.log(`... and urls`);
   console.log(`seeded successfully`);
 }
 
